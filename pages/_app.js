ac866b4c7a144f8e245d87b670da077bc2438d77
Michael Liendo <mtliendo@gmail.com>
2021-08-15 21:14:07 -0500

    add ssr support

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
-
+import Amplify from 'aws-amplify'
+import config from '../src/aws-exports'
+Amplify.configure({ ...config, ssr: true })
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
