6b49873dfe96dfd58f8b5fcbf89b2ef5332eadf8
Michael Liendo <mtliendo@gmail.com>
2021-08-15 21:20:18 -0500

    fix broken path

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import Amplify from 'aws-amplify'
-import config from '../src/aws-exports'
+import config from '../aws-exports'
 Amplify.configure({ ...config, ssr: true })
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
